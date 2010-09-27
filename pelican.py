c6cef328c6abccf3c72d1ca2fec8095299fb576a
Alexis Metaireau <ametaireau@gmail.com>
2010-09-27 19:24:25 +0100

    Init the repository and add an article about pelican

---
+++ pelican.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+THEME = '/home/alexis/dev/python/pelican/samples/themes/notmyidea' 
+AUTHOR = u'Alexis Métaireau'
+
+BLOGNAME = u"Alexis' log" 
+# BLOGURL = 'http://blog.notmyidea.org'
+
+BLOGROLL = (('Biologeek', 'http://biologeek.org'),
+            ('Filyb', "http://filyb.info/"),
+            ('Libert-fr', "http://www.libert-fr.com"),
+            ('N1k0', "http://prendreuncafe.com/blog/"),
+            (u'Tarek Ziadé', "http://ziade.org/blog"),
+            ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
+
+SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
+          ('lastfm', 'http://lastfm.com/user/akounet'),)
