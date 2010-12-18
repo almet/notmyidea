40ccf84c6640aa2b20eb183547df7b5a11e7d248
Alexis Metaireau <alexis@notmyidea.org>
2010-12-18 20:20:41 +0000

    Format the settings.

--- pelican.conf.py
+++ pelican.conf.py
@@ -5,11 +5,11 @@ SITENAME = u"Alexis' log"
 DISQUS_SITENAME = "blog-notmyidea"
 
 LINKS = (('Biologeek', 'http://biologeek.org'),
-            ('Filyb', "http://filyb.info/"),
-            ('Libert-fr', "http://www.libert-fr.com"),
-            ('N1k0', "http://prendreuncafe.com/blog/"),
-            (u'Tarek Ziadé', "http://ziade.org/blog"),
-            ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
+         ('Filyb', "http://filyb.info/"),
+         ('Libert-fr', "http://www.libert-fr.com"),
+         ('N1k0', "http://prendreuncafe.com/blog/"),
+         (u'Tarek Ziadé', "http://ziade.org/blog"),
+         ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
 
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
           ('lastfm', 'http://lastfm.com/user/akounet'),
