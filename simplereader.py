47dc59cdd867800451960e2757dc2300e5506e76
Alexis M <alexis@notmyidea.org>
2019-11-20 13:56:44 +0100

    Handle citations

--- simplereader.py
+++ simplereader.py
@@ -1,8 +1,24 @@
 from pelican import signals
-from pelican.readers import MarkdownReader, Category
+from pelican.readers import MarkdownReader, Category, Markdown, pelican_open
 from pelican.utils import get_date, slugify
 
+from markdown.preprocessors import Preprocessor
+from datefinder import find_dates
+
 import os.path
+from datetime import datetime
+
+
+class BlockquotesPreprocessor(Preprocessor):
+    def run(self, lines):
+        new_lines = []
+        for line in lines:
+            if line.startswith(">"):
+                new_lines.append("&nbsp;")
+                new_lines.append(line)
+            else:
+                new_lines.append(line)
+        return new_lines
 
 
 class SimpleReader(MarkdownReader):
@@ -14,8 +30,22 @@ class SimpleReader(MarkdownReader):
         super(SimpleReader, self).__init__(*args, **kwargs)
         self.settings["MARKDOWN"]["extensions"].append("markdown.extensions.toc")
 
-    def read(self, filename):
-        content, metadata = super(SimpleReader, self).read(filename)
+    def read(self, source_path):
+        self._source_path = source_path
+        self._md = Markdown(**self.settings["MARKDOWN"])
+        if "Lectures" in source_path:
+            self._md.preprocessors.register(
+                BlockquotesPreprocessor(self._md), "blockquotes", 10
+            )
+
+        with pelican_open(source_path) as text:
+            content = self._md.convert(text)
+
+        if hasattr(self._md, "Meta"):
+            metadata = self._parse_metadata(self._md.Meta)
+        else:
+            metadata = {}
+
         # Add the TOC to the metadata.
         if len(self._md.toc) > 300:
             metadata["table_of_contents"] = self._md.toc
@@ -28,9 +58,11 @@ class SimpleReader(MarkdownReader):
                 '<h1 id="{id}">{name}</h1>'.format(**first_title), ""
             )
 
-        # Get the date from the filename.
-        parts = os.path.splitext(os.path.basename(filename))[0].split("-")
-        if len(parts) >= 3:
+        # Get the date from the filename, if possible.
+        parts = os.path.splitext(os.path.basename(source_path))[0].split("-")
+        if "read_on" in metadata:
+            metadata["date"] = datetime.strptime(metadata["read_on"], "%B %Y")
+        elif len(parts) >= 3:
             metadata["date"] = get_date("-".join(parts[:3]))
 
         if "slug" not in metadata:
@@ -38,7 +70,9 @@ class SimpleReader(MarkdownReader):
                 metadata["title"], self.settings.get("SLUG_REGEX_SUBSTITUTIONS", [])
             )
 
-        category = os.path.basename(os.path.abspath(os.path.join(filename, os.pardir)))
+        category = os.path.basename(
+            os.path.abspath(os.path.join(source_path, os.pardir))
+        )
         metadata["category"] = self.process_metadata("category", category)
         return content, metadata
 
