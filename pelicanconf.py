7b5c401ece90632d7be7fc2f2431978db508e322
Alexis M <alexis@notmyidea.org>
2019-12-07 16:09:16 +0100

    Anonymity

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,19 +15,13 @@ LOCALE = "fr_FR.utf8"
 
 DEFAULT_DATE_FORMAT = "%d %B %Y"
 LINKS = [
-    ("Brasserie du Vieux Singe", "https://www.vieuxsinge.com"),
-    ("A propos", "http://blog.notmyidea.org/pages/about.html"),
-    ("Messages courts", "https://twitter.com/ametaireau"),
     ("Code", "https://github.com/almet"),
 ]
 
-PIWIK_SERVER_URL = "//tracker.notmyidea.org/"
-FIREFOX_BOOKMARKS_PATH = (
-    "/home/alexis/.mozilla/firefox/fymmscw3.default/bookmarkbackups/"
-)
-PIWIK_SITE_ID = 3
 PLUGIN_PATHS = ["."]
-PLUGINS = ["simplereader", ]
+PLUGINS = [
+    "simplereader",
+]
 
 CACHE_OUTPUT_DIRECTORY = "cache"
 CACHE_DOMAIN = "/cache/"
